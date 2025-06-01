320ff2e9cc679fc33af725661c27a34dd2fd5e4a
tsyonatanp <s0547274527@gmail.com>
2025-06-01 17:21:05 +0300

    Delete src/app/src/app/metadata.ts

--- src/app/src/app/metadata.ts
+++
@@ -1,6 +0,0 @@
-import type { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'Redy Food - אוכל מוכן במשלוח',
-  description: 'הזמנת אוכל מוכן טרי לפי משקל עם משלוח עד הבית',
-} 
