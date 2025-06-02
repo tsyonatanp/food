a7e73f5ac881b98b1b7ff3ceb2e04681b83ba8a6
tsyonatanp <s0547274527@gmail.com>
2025-06-02 21:06:48 +0300

    Delete src directory

--- src/app/src/app/src/lib/fetchMenu.ts
+++
@@ -1,5 +0,0 @@
-export async function fetchMenu() {
-  const res = await fetch('https://opensheet.elk.sh/1xTlTECzbxdDVu6SSnoQnN_GtL39b7-ZzhgcIvaLyljU/Menu');
-  if (!res.ok) throw new Error('Failed to fetch menu');
-  return res.json();
-} 
--- src/app/src/lib/fetchCategories.ts
+++
@@ -1,7 +0,0 @@
-export async function fetchCategories() {
-  const res = await fetch('https://opensheet.elk.sh/1xTlTECzbxdDVu6SSnoQnN_GtL39b7-ZzhgcIvaLyljU/MenuCategories');
-  if (!res.ok) {
-    throw new Error('Failed to fetch categories');
-  }
-  return res.json();
-} 
