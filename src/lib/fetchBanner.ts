1d631506598173cb6dd7bf99b1bb0785d26a13d1
tsyonatanp <s0547274527@gmail.com>
2025-06-03 13:55:11 +0300

    Add files via upload

--- src/lib/fetchBanner.ts
+++ src/lib/fetchBanner.ts
@@ -1,5 +1,5 @@
 export async function fetchBanner() {
-  const res = await fetch('https://opensheet.elk.sh/1xTITECxbxDUv6SsnoQn_GtL39b7-ZzhgcIvaLyjljU/Banner');
+  const res = await fetch('https://opensheet.elk.sh/1xTlTECzbxdDVu6SSnoQnN_GtL39b7-ZzhgcIvaLyljU/Banner');
   if (!res.ok) throw new Error('Failed to fetch banner');
   return res.json();
 } 
